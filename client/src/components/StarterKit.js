f5bd4d12f89048b9ca2751c00537fcce9c4bab68
Steven Stot <sstott09@yahoo.com>
2020-02-24 11:22:40 -0700

    Added Client/SRC/Component/StarterKit.js

---
+++ client/src/components/StarterKit.js
@@ -0,0 +1,37 @@
+// import React, { Component } from 'react';
+// import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+// import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import uuid from 'uuid';
+
+// class StarterKit extends Component {
+//     state = {
+//         items: [
+//             { id: uuid(), name: 'Leash'},
+//             { id: uuid(), name: 'Harness'},
+//             { id: uuid(), name: 'Bedding'},
+//             { id: uuid(), name: 'Dog Bowl'},
+//         ]
+//     }
+
+// render() {
+//     const { items } = this.state;
+//     return(
+//             <Container>
+//                 <Button
+//                     color="dark"
+//                     style={{marginBottom: '2rem'}}
+//                     onClick={() => {
+//                         const name = prompt ('Enter Item');
+//                         if(name) {
+//                             this.setState(state => ({
+//                                 items: [...state.items, { id: uuid(), name }]
+//                             }));
+//                         }
+//                     }}
+//                 >Add Item</Button>
+//             </Container>
+//         );
+//     }
+// }
+
+// export default StarterKit;
